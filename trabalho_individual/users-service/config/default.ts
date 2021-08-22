10f8ff9db925ff308352091baa8558b9944aba9f
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2021-08-22 13:00:20 -0300

    Adicionando dockerização em todos os serviços no serviço individual

--- trabalho_individual/users-service/config/default.ts
+++ trabalho_individual/users-service/config/default.ts
@@ -1,2 +1,2 @@
-export const USERS_SERVICE_DB_URL = "mysql://root:password@users-service-db/db";
+export const USERS_SERVICE_DB_URL = "mysql://root:password@mysqldb/db";
 export const USER_SESSION_EXPIRY_HOURS = 1;
